cd7472d29ced1e6f58b6783672d35a20b11e4c33
JPGonzalezT <juapabgonzalez@utp.edu.co>
2024-11-26 15:02:24 -0500

    probando python

---
+++ black_hat_python/test.py
@@ -0,0 +1,14 @@
+def sum(number_one, number_two):
+    number_one = int(number_one)
+    number_two = int(number_two)
+
+    result = number_one + number_two
+    return result
+
+def convert_to_integer(number_string):
+    convert_integer = int(number_string)
+    return convert_integer
+
+answer = sum("1","2")
+
+print(answer)
